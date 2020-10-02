027ff28facd154c21ad98c6b3b850053747773c0
Jonatan "jaw" Wallmander <jonatan@vovoid.com>
2020-10-02 11:37:58 +0200

    fix for c++20

--- src/FTContour.cpp
+++ src/FTContour.cpp
@@ -32,6 +32,10 @@
 #define _USE_MATH_DEFINES
 #include <math.h>
 
+#ifndef M_PI
+#define M_PI    3.14159265358979323846264338327950288   /**< pi */
+#endif
+
 static const unsigned int BEZIER_STEPS = 5;
 
 
